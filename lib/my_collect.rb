b4cf779fca9d4e468f18840de8a77947991e409c
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:20:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,4 +10,9 @@ end
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
-end 
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
