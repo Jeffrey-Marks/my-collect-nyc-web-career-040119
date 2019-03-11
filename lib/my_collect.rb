7e9c4dd44270b5d15f93aff4d6d9a533b63c72a2
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:32:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  output_array = []
+  i = 0
+  while i < array.length
+    output_array.push(array[i])
+    output_array[i] = yield output_array[i]
+    # output_array[i] = yield output_array.push(array[i])[i]
+    i += 1
+  end
+  output_array
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
