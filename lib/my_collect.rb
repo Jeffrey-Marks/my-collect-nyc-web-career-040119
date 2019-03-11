9afdf8402e3689ace073a489c063c08b5a70f45a
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:20:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,6 +7,7 @@ def my_collect(array)
   end
 end
 
-# my_collect(collection) do |x|
-  
-# end
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end 
