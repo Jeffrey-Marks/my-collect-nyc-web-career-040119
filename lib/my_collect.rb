538d2b36901b1cda8ed16f362e23404c0d6ad5cc
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:31:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,13 +2,15 @@ def my_collect(array)
   output_array = []
   i = 0
   while i < array.length
-    yield output_array.push(array[i])[i]
+    output_array.push(array[i])
+    output_array[i] = yield output_array[i]
     i += 1
   end
   output_array
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
 my_collect(array) do |name|
   name.split(" ").first
 end
